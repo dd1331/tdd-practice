028a32c305da31364007244c43501a50ad54f4f0
Chalie Choi <ddd1331@gmail.com>
2021-05-03 01:16:33 +0900

    single player game

--- src/AppModel.ts
+++ src/AppModel.ts
@@ -2,18 +2,50 @@ import { PositiveIntegerGenerator } from "./PositiveIntegerGenerator";
 const NEW_LINE = '\n';
 
 export class AppModel {
-	private completed: boolean = false;
-	constructor(private generator:PositiveIntegerGenerator) {}
+	
+	constructor(
+		private generator: PositiveIntegerGenerator,
+		private output: string = '1: Single player game' + NEW_LINE + '2: Multiplayer game' + NEW_LINE + '3: Exit' + NEW_LINE + 'Enter selection: ',
+		private completed: boolean = false,
+		private answer: number = generator.generateLessThanOrEqualToHundred(),
+		private singPlayerMode: boolean = false
+	) {}
 
 	isCompleted(): boolean {
 		return this.completed;
 	}
 	flushOutput(): string {
-			return '1: Single player game' + NEW_LINE + '2: Multiplayer game' + NEW_LINE + '3: Exit' + NEW_LINE + 'Enter selection: ';
+			return this.output;
+			
 	}
 
 	processInput(input: string): void {
-		if (input === '3') {
+		if (this.singPlayerMode) {
+			this.processSinglePlayerGame(input);
+		} else {
+			this.processModeSelection(input);
+		}
+	}
+
+	private processSinglePlayerGame(input: string) {
+		const guess = parseInt(input);
+		if (guess < this.answer) {
+			this.output = 'Your guess is too low' + NEW_LINE + 'Enter your guess: ';
+		}
+		else if (guess > this.answer) {
+			this.output = 'Your guess is too high' + NEW_LINE + 'Enter your guess: ';
+		}
+		else {
+			this.output = 'Correct';
+		}
+	}
+
+	private processModeSelection(input: string) {
+		if (input === '1') {
+			this.output = 'Single player game' + NEW_LINE + 'I am thinking of a number between 1 and 100' + NEW_LINE + 'Enter your guess: ';
+			this.singPlayerMode = true;
+		}
+		else {
 			this.completed = true;
 		}
 	}
--- src/test/AppModel.spec.ts
+++ src/test/AppModel.spec.ts
@@ -1,3 +1,4 @@
+import each from 'jest-each';
 import { AppModel } from "../AppModel"
 import { PositiveIntegerGeneratorStub } from "../PositiveIntegerGeneratorStub";
 
@@ -16,7 +17,6 @@ describe('AppModel', () => {
 		const sut = new AppModel(new PositiveIntegerGeneratorStub([50]));
 		const actual: string = sut.flushOutput();
 		
-		
 		expect(actual).toEqual('1: Single player game' + NEW_LINE + '2: Multiplayer game' + NEW_LINE + '3: Exit' + NEW_LINE + 'Enter selection: ')
 		
 	})
@@ -29,4 +29,62 @@ describe('AppModel', () => {
 		const actual: boolean = sut.isCompleted();
 		expect(actual).toBeTruthy();
 	})
+
+	it('sut prints single player game start message', () => {
+		const sut = new AppModel(new PositiveIntegerGeneratorStub([50]));
+		sut.flushOutput();
+		sut.processInput('1');
+		
+		const actual: string = sut.flushOutput();
+		// 'Single player game' + NEW_LINE + 'I am thinking of a number between 1 and 100' + NEW_LINE + 'Enter your guess: '
+		expect(actual).toEqual('Single player game' + NEW_LINE + 'I am thinking of a number between 1 and 100' + NEW_LINE + 'Enter your guess: ')
+	})
+	
+	const params = [
+		[50, 40],
+		[30, 29],
+		[89, 9],
+	]
+	each(params).it('sut prints too low message in single player game', (answer: number, guess: number) => {
+		const sut = new AppModel(new PositiveIntegerGeneratorStub([answer]));
+		sut.processInput('1');
+		sut.flushOutput();
+		sut.processInput(guess.toString());
+		
+		const actual: string = sut.flushOutput();
+		
+		expect(actual).toEqual('Your guess is too low' + NEW_LINE + 'Enter your guess: ');
+	});
+	const params2 = [
+		[50, 69],
+		[30, 31],
+		[89, 93],
+	];
+	each(params2).it('sut prints too high message in single player game', (answer: number, guess: number) => {
+		const sut = new AppModel(new PositiveIntegerGeneratorStub([answer]));
+		sut.processInput('1');
+		sut.flushOutput();
+		sut.processInput(guess.toString());
+		
+		const actual: string = sut.flushOutput();
+		
+		expect(actual).toEqual('Your guess is too high' + NEW_LINE + 'Enter your guess: ');
+	})
+	
+	each([1, 3, 10, 30]).it('sut prints correct message in single player game', (answer: number) => {
+		const sut = new AppModel(new PositiveIntegerGeneratorStub([answer]));
+		sut.processInput('1');
+		sut.flushOutput();
+
+		const guess = answer;
+
+		sut.processInput(guess.toString());
+		
+		const actual: string = sut.flushOutput();
+		
+		expect(actual).toEqual('Correct');
+	})
+
+	
+	
 })
