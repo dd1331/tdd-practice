720b5ba357ac2087cbc66c93dcf3d58d5f81f996
Chalie Choi <ddd1331@gmail.com>
2021-05-03 23:55:01 +0900

    single player game

--- src/AppModel.ts
+++ src/AppModel.ts
@@ -2,13 +2,15 @@ import { PositiveIntegerGenerator } from "./PositiveIntegerGenerator";
 const NEW_LINE = '\n';
 
 export class AppModel {
-	
+	private static readonly SELECT_MODE_MESSAGE = '1: Single player game' + NEW_LINE + '2: Multiplayer game' + NEW_LINE + '3: Exit' + NEW_LINE + 'Enter selection: ';
+
 	constructor(
 		private generator: PositiveIntegerGenerator,
-		private output: string = '1: Single player game' + NEW_LINE + '2: Multiplayer game' + NEW_LINE + '3: Exit' + NEW_LINE + 'Enter selection: ',
+		private output: string = AppModel.SELECT_MODE_MESSAGE,
 		private completed: boolean = false,
-		private answer: number = generator.generateLessThanOrEqualToHundred(),
-		private singPlayerMode: boolean = false
+		private answer: number = 0,
+		private singPlayerMode: boolean = false,
+		private tries: number = 0
 	) {}
 
 	isCompleted(): boolean {
@@ -29,6 +31,7 @@ export class AppModel {
 
 	private processSinglePlayerGame(input: string) {
 		const guess = parseInt(input);
+		this.tries += 1;
 		if (guess < this.answer) {
 			this.output = 'Your guess is too low' + NEW_LINE + 'Enter your guess: ';
 		}
@@ -36,14 +39,18 @@ export class AppModel {
 			this.output = 'Your guess is too high' + NEW_LINE + 'Enter your guess: ';
 		}
 		else {
-			this.output = 'Correct';
+			this.output = 'Correct' + (this.tries) + (this.tries === 1 ? ' guess' : ' guesses') + NEW_LINE
+			+ '1: Single player game' + NEW_LINE + '2: Multiplayer game' + NEW_LINE + '3: Exit' + NEW_LINE + 'Enter selection: ';
+			this.tries = 0;
+			this.singPlayerMode = false;
 		}
 	}
-
+	
 	private processModeSelection(input: string) {
 		if (input === '1') {
 			this.output = 'Single player game' + NEW_LINE + 'I am thinking of a number between 1 and 100' + NEW_LINE + 'Enter your guess: ';
 			this.singPlayerMode = true;
+			this.answer = this.generator.generateLessThanOrEqualToHundred();
 		}
 		else {
 			this.completed = true;
--- src/test/AppModel.spec.ts
+++ src/test/AppModel.spec.ts
@@ -82,7 +82,68 @@ describe('AppModel', () => {
 		
 		const actual: string = sut.flushOutput();
 		
-		expect(actual).toEqual('Correct');
+		expect(actual).toContain('Correct');
+	})
+	
+	each([1, 10, 100]).it('sut prints guess count if single player game is finished', (fails) => {
+		const sut = new AppModel(new PositiveIntegerGeneratorStub([50]));
+		sut.processInput('1');
+		for (let i = 0; i < fails; i++) {
+			sut.processInput('30');
+		}
+		sut.flushOutput();
+		sut.processInput('50');
+		
+		const actual: string = sut.flushOutput();
+		
+		expect(actual).toContain((fails + 1) + ' guesses' + NEW_LINE);
+	})
+	
+	it('sut prints one guess if single player game is done', () => {
+		const sut = new AppModel(new PositiveIntegerGeneratorStub([50]));
+		sut.processInput('1');
+		sut.flushOutput();
+		sut.processInput('50');
+
+		const actual = sut.flushOutput();
+		
+		expect(actual).toContain('1 guess');
+	})
+	
+	it('sut prints select mode message if single player game is finished', () => {
+		const sut = new AppModel(new PositiveIntegerGeneratorStub([50]));
+		sut.processInput('1');
+		sut.flushOutput();
+		sut.processInput('50');
+		
+		const actual = sut.flushOutput();
+		
+		expect(actual).toContain('1: Single player game' + NEW_LINE + '2: Multiplayer game' + NEW_LINE + '3: Exit' + NEW_LINE + 'Enter selection: ');
+	})
+	
+	it('sut returns to mode selection if single player game is finished', () => {
+		const sut = new AppModel(new PositiveIntegerGeneratorStub([50]));
+		sut.processInput('1');
+		sut.processInput('50');
+		sut.processInput('3');
+		
+		const actual = sut.isCompleted();
+		expect(actual).toBeTruthy();
+	})
+
+	each(['100,10,1']).it('sut generates answer for each game', (source: string) => {
+		const answers: number[] = source.split(',').map(c => parseInt(c.trim()));
+		const sut = new AppModel(new PositiveIntegerGeneratorStub(answers));
+		answers.forEach(answer => {
+			sut.processInput('1');
+			sut.flushOutput();
+			sut.processInput(answer.toString());
+		});
+
+		const actual = sut.flushOutput();
+
+		expect(actual).toContain('Correct');
+	
 	})
 
 	
