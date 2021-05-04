6abc41ac0d09836d81d6ab0ff3d4005b2f43968a
Chalie Choi <ddd1331@gmail.com>
2021-05-04 12:29:54 +0900

    user processor

--- src/AppModel.ts
+++ src/AppModel.ts
@@ -1,8 +1,11 @@
 import { PositiveIntegerGenerator } from "./PositiveIntegerGenerator";
 const NEW_LINE = '\n';
 
+type Processor = (input: string) => Processor;
+
 export class AppModel {
 	private static readonly SELECT_MODE_MESSAGE = '1: Single player game' + NEW_LINE + '2: Multiplayer game' + NEW_LINE + '3: Exit' + NEW_LINE + 'Enter selection: ';
+	private processor: Processor
 
 	constructor(
 		private generator: PositiveIntegerGenerator,
@@ -10,8 +13,11 @@ export class AppModel {
 		private completed: boolean = false,
 		private answer: number = 0,
 		private singPlayerMode: boolean = false,
-		private tries: number = 0
-	) {}
+		private tries: number = 0,
+		// private processor: Processor
+	) {
+		this.processor  = this.processModeSelection()
+	}
 
 	isCompleted(): boolean {
 		return this.completed;
@@ -22,38 +28,49 @@ export class AppModel {
 	}
 
 	processInput(input: string): void {
-		if (this.singPlayerMode) {
-			this.processSinglePlayerGame(input);
-		} else {
-			this.processModeSelection(input);
+		this.processor = this.processor(input);
+		// if (this.singPlayerMode) {
+		// 	this.processSinglePlayerGame(input);
+		// } else {
+			// 	this.processModeSelection(input);
+			// }
 		}
-	}
-
-	private processSinglePlayerGame(input: string) {
-		const guess = parseInt(input);
-		this.tries += 1;
-		if (guess < this.answer) {
-			this.output = 'Your guess is too low' + NEW_LINE + 'Enter your guess: ';
-		}
-		else if (guess > this.answer) {
-			this.output = 'Your guess is too high' + NEW_LINE + 'Enter your guess: ';
-		}
-		else {
-			this.output = 'Correct' + (this.tries) + (this.tries === 1 ? ' guess' : ' guesses') + NEW_LINE
-			+ '1: Single player game' + NEW_LINE + '2: Multiplayer game' + NEW_LINE + '3: Exit' + NEW_LINE + 'Enter selection: ';
-			this.tries = 0;
-			this.singPlayerMode = false;
+		
+		// private processSinglePlayerGame(input: string) {
+	private getSinglePlayerGameProcessor(tries: number): Processor {
+		return input => {
+			const guess = parseInt(input);
+			// this.tries += 1;
+			if (guess < this.answer) {
+				this.output = 'Your guess is too low' + NEW_LINE + 'Enter your guess: ';
+				return this.getSinglePlayerGameProcessor(tries + 1);
+			}
+			else if (guess > this.answer) {
+				this.output = 'Your guess is too high' + NEW_LINE + 'Enter your guess: ';
+				return this.getSinglePlayerGameProcessor(tries + 1);
+			}
+			else {
+				this.output = 'Correct' + (tries) + (tries === 1 ? ' guess' : ' guesses') + NEW_LINE
+				+ '1: Single player game' + NEW_LINE + '2: Multiplayer game' + NEW_LINE + '3: Exit' + NEW_LINE + 'Enter selection: ';
+				this.tries = 0;
+				this.singPlayerMode = false;
+				return this.processModeSelection()
+			}
 		}
 	}
 	
-	private processModeSelection(input: string) {
-		if (input === '1') {
-			this.output = 'Single player game' + NEW_LINE + 'I am thinking of a number between 1 and 100' + NEW_LINE + 'Enter your guess: ';
-			this.singPlayerMode = true;
-			this.answer = this.generator.generateLessThanOrEqualToHundred();
-		}
-		else {
-			this.completed = true;
+	private processModeSelection(): Processor {
+		return (input: string) => {
+			if (input === '1') {
+				this.output = 'Single player game' + NEW_LINE + 'I am thinking of a number between 1 and 100' + NEW_LINE + 'Enter your guess: ';
+				this.singPlayerMode = true;
+				this.answer = this.generator.generateLessThanOrEqualToHundred();
+				return this.getSinglePlayerGameProcessor(1);
+			}
+			else {
+				this.completed = true;
+				return this.processor
+			}
 		}
 	}
 }
