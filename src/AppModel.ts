dc8b1013a39e4a97574d252a7d611250f58a53db
Chalie Choi <ddd1331@gmail.com>
2021-05-04 17:48:16 +0900

    multiplayer game

--- src/AppModel.ts
+++ src/AppModel.ts
@@ -19,14 +19,35 @@ export class AppModel {
 	isCompleted(): boolean {
 		return this.completed;
 	}
+
 	flushOutput(): string {
-			return this.output;
-			
+			const result = this.output;
+			this.output = '';
+			return result;
 	}
 
 	processInput(input: string): void {
 		this.processor = this.processor(input);
 	}
+
+	private processModeSelection(): Processor {
+		return (input: string) => {
+			if (input === '1') {
+				this.output = 'Single player game' + NEW_LINE + 'I am thinking of a number between 1 and 100' + NEW_LINE + 'Enter your guess: ';
+				const answer = this.generator.generateLessThanOrEqualToHundred();
+				return this.getSinglePlayerGameProcessor(answer, 1);
+			} else if ( input === '2') {
+				this.output = 'Multiplayer game' + NEW_LINE + 'Enter player names with commas:';
+				const answer = this.generator.generateLessThanOrEqualToHundred();
+				return this.startMultiPlayerGameProcessor(answer);
+			}
+			else {
+				this.completed = true;
+				return this.processor
+			}
+		}
+	}
+
 	private getSinglePlayerGameProcessor(answer: number, tries: number): Processor {
 		return input => {
 			const guess = parseInt(input);
@@ -48,17 +69,26 @@ export class AppModel {
 		}
 	}
 	
-	private processModeSelection(): Processor {
-		return (input: string) => {
-			if (input === '1') {
-				this.output = 'Single player game' + NEW_LINE + 'I am thinking of a number between 1 and 100' + NEW_LINE + 'Enter your guess: ';
-				const answer = this.generator.generateLessThanOrEqualToHundred();
-				return this.getSinglePlayerGameProcessor(answer, 1);
+	private startMultiPlayerGameProcessor(answer: number): Processor {
+		return input => {
+			const players = input.split(',')
+			this.output = 'I am thinking of a number between 1 and 100' 
+			return this.getMultiPlayerGameProcessor(players, 1, answer);
+		}
+	}
+	private getMultiPlayerGameProcessor(players: string[], tries: number, answer: number): Processor {
+		const player = players[(tries - 1) % players.length];
+		this.output += ' Enter ' + player + "'s guess: "
+		return input => {
+			const guess = parseInt(input);
+			if (guess < answer) {
+				this.output = player + "'s guess is too low" + NEW_LINE;
+			} else if (guess > answer) {
+				this.output = player + "'s guess is too high" + NEW_LINE;
+			} else {
+				this.output = 'correct! ' + player +' wins' + NEW_LINE;
 			}
-			else {
-				this.completed = true;
-				return this.processor
-			}
+			return this.getMultiPlayerGameProcessor(players, tries + 1, answer)
 		}
 	}
 }
--- src/test/AppModel.spec.ts
+++ src/test/AppModel.spec.ts
@@ -139,13 +139,172 @@ describe('AppModel', () => {
 			sut.flushOutput();
 			sut.processInput(answer.toString());
 		});
+		
+		const actual = sut.flushOutput();
+		
+		expect(actual).toContain('Correct');
+	})
+	
+	it('sut prints multiplayer game setup message', () => {
+		const sut = new AppModel(new PositiveIntegerGeneratorStub([50]));
+		sut.flushOutput();
+		sut.processInput('2');
+		
+		const actual = sut.flushOutput();
+		
+		expect(actual).toEqual('Multiplayer game' + NEW_LINE + 'Enter player names with commas:')
+	})
+	
+	it('sut prints multiplayer game start message', () => {
+		const sut = new AppModel(new PositiveIntegerGeneratorStub([50]));
+
+		sut.processInput('2');
+		sut.flushOutput();
+		sut.processInput('foo, bar');
+		
+		const actual = sut.flushOutput();
+		
+		expect(actual).toContain('I am thinking of a number between 1 and 100');
+		
+	})
+	
+	const params3 = [
+		['foo', 'bar', 'baz'],
+		['bar', 'baz', 'foo'],
+		['baz', 'foo', 'bar']
+	];
+	each(params3).it('sut prompts first player name', (player: string, player2: string, player3: string) => {
+		const sut = new AppModel(new PositiveIntegerGeneratorStub([50]));
+		sut.processInput('2');
+		sut.flushOutput();
+		sut.processInput([player, player2, player3].join(','))
+		
+		const actual = sut.flushOutput();
+		
+		expect(actual).toContain('Enter ' + player + "'s guess: ")
+	})
+	
+	each(params3).it('sut prompts second player name', (player: string, player2: string, player3: string) => {
+		const sut = new AppModel(new PositiveIntegerGeneratorStub([50]));
+		sut.processInput('2');
+		sut.flushOutput();
+		sut.processInput([player, player2, player3].join(','))
+		sut.flushOutput();
+		sut.processInput('10');
+		
+		const actual = sut.flushOutput();
+		
+		expect(actual).toContain(' Enter ' + player2 + "'s guess: ")
+
+	})
+	each(params3).it('sut prompts third player name', (player: string, player2: string, player3: string) => {
+		const sut = new AppModel(new PositiveIntegerGeneratorStub([50]));
+		sut.processInput('2');
+		sut.flushOutput();
+		sut.processInput([player, player2, player3].join(','))
+		sut.flushOutput();
+		sut.processInput('10');
+		sut.flushOutput();
+		sut.processInput('10');
+		
+		const actual = sut.flushOutput();
+		expect(actual).toContain(' Enter ' + player3 + "'s guess: ")
+	})
+	each(params3).it('sut rounds players', (player: string, player2: string, player3: string) => {
+		const sut = new AppModel(new PositiveIntegerGeneratorStub([50]));
+		sut.processInput('2');
+		sut.processInput([player, player2, player3].join(','))
+		sut.processInput('10');
+		sut.processInput('10');
+		sut.flushOutput();
+		sut.processInput('10');
+		
+		const actual = sut.flushOutput();
+		expect(actual.endsWith(' Enter ' + player + "'s guess: ")).toBeTruthy();
+	})
+	
+	const params4 = [
+		['50', '40', '1', 'foo'],
+		['30', '29', '2', 'bar'],
+	];
+	
+	each(params4).it('sut prints too low message in multiplayer game', 
+	(answer: number, guess: number, fails: number, lastPlayer: string) => {
+		const sut = new AppModel(new PositiveIntegerGeneratorStub([answer]));
+		sut.processInput('2');
+		sut.processInput('foo,bar,baz');
+		for (let i = 0; i < fails - 1; i++) {
+			sut.processInput(guess.toString())
+		}
+		sut.flushOutput();
+		sut.processInput(guess.toString());
+		
+		const actual = sut.flushOutput()
+		expect(actual.startsWith(lastPlayer + "'s guess is too low" + NEW_LINE)).toBeTruthy();
+	})
+	const params5 = [
+		['50', '55', '1', 'foo'],
+		['30', '33', '2', 'bar'],
+	];
+	
+	each(params5).it('sut prints too high message in multiplayer game', 
+	(answer: number, guess: number, fails: number, lastPlayer: string) => {
+		const sut = new AppModel(new PositiveIntegerGeneratorStub([answer]));
+		sut.processInput('2');
+		sut.processInput('foo,bar,baz');
+		for (let i = 0; i < fails - 1; i++) {
+			sut.processInput(guess.toString())
+		}
+		sut.flushOutput();
+		sut.processInput(guess.toString());
+		
+		const actual = sut.flushOutput()
+		expect(actual.startsWith(lastPlayer + "'s guess is too high" + NEW_LINE)).toBeTruthy();
+	})
+	
+	each([1, 10, 100]).it('sut prints correct message in multiplayer game', (answer: number) => {
+		const sut = new AppModel(new PositiveIntegerGeneratorStub([answer]));
+		sut.processInput('2');
+		sut.processInput('foo,bar,baz');
+		sut.flushOutput();
+		
+		const guess = answer;
+		
+		sut.processInput(guess.toString())
+		
+		const actual = sut.flushOutput();
+		
+		expect(actual.startsWith('correct! ')).toBeTruthy();
+	})
+	
+	const params6 = [
+		[0, 'foo'],
+		[1, 'bar'],
+		[2, 'baz'],
+		[99, 'foo'],
+		[100, 'bar'],
+	];
+	
+	each(params6).it('sut prints winner if muliplayer game is finished',
+	(fails: number, winner: string) => {
+		const sut = new AppModel(new PositiveIntegerGeneratorStub([50]));
+		sut.processInput('2');
+		sut.processInput('foo,bar,baz');
+		for (let i = 0; i < fails; i++) {
+			sut.processInput('30')
+		}
+		sut.flushOutput();
+		sut.processInput('50');
 
 		const actual = sut.flushOutput();
+		console.log(actual)
 
-		expect(actual).toContain('Correct');
-	
+		expect(actual).toContain(winner + ' wins' + NEW_LINE);
+		
+		
 	})
-
+	
+	
 	
 	
 })
